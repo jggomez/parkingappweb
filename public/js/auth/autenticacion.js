392d2e0f1c9a78512d692384d9a7f913e9794ac7
Juan Guillermo Gomez Torres <juan.gomez01@gmail.com>
2019-07-23 16:21:38 -0500

    version con storage, auth, firestore

---
+++ public/js/auth/autenticacion.js
@@ -0,0 +1,84 @@
+class Autenticacion {
+
+  async autEmailPass(email, password) {
+    try {
+      const result = await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+
+      if (!result.user.emailVerified) {
+        await firebase.auth().signOut();
+        return false;
+      }
+      return true;
+
+    } catch (error) {
+      console.error(error)
+      Materialize.toast(error.message, 4000)
+    }
+  }
+
+  async crearCuentaEmailPass(email, password, nombres) {
+    try {
+      const result = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+
+      await result.user.updateProfile({
+        displayName: nombres
+      });
+
+      const configuracion = {
+        url: 'https://parking-f75e3.firebaseapp.com/'
+      }
+
+      await result.user.sendEmailVerification(configuracion);
+
+      await firebase.auth().signOut();
+
+      return true;
+
+    } catch (error) {
+      console.error(error)
+      Materialize.toast(error.message, 4000)
+    }
+  }
+
+  async authCuentaGoogle() {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider()
+
+      const result = await firebase.auth().signInWithPopup(provider)
+
+      if (result) {
+        return result.user;
+      }
+
+      return undefined;
+    } catch (error) {
+      console.error(error)
+      Materialize.toast(`Error al autenticarse con google: ${error} `, 4000)
+    }
+  }
+
+  async authCuentaFacebook() {
+    try {
+      const provider = new firebase.auth.FacebookAuthProvider()
+
+      const result = await firebase.auth().signInWithPopup(provider)
+
+      if (result) {
+        return result.user;
+      }
+
+      return undefined;
+    } catch (error) {
+      console.error(error)
+      Materialize.toast(`Error al autenticarse con google: ${error} `, 4000)
+    }
+  }
+
+  authTwitter() {
+    // TODO: Crear auth con twitter
+  }
+}
---
+++ public/js/parqueaderos/parqueadero.js
@@ -0,0 +1,179 @@
+class Parqueadero {
+
+  constructor() {
+    this.db = firebase.firestore();
+  }
+
+  async crearEntrada(
+    uid,
+    idParqueadero,
+    nombreCliente,
+    placa,
+    celularCliente,
+    observacion,
+    imagenLink) {
+    try {
+      const refDoc = await this.db
+        .collection('entradas')
+        .add({
+          uid: uid,
+          idparqueadero: idParqueadero,
+          nombrecliente: nombreCliente,
+          celularcliente: celularCliente,
+          placa: placa,
+          observacion: observacion,
+          imagenlink: imagenLink,
+          fecha: firebase.firestore.FieldValue.serverTimestamp()
+        })
+
+      await this.db
+        .collection('parqueaderos')
+        .doc(idParqueadero)
+        .update({
+          "libre": false
+        });
+
+      console.log(`Id de la entrada => ${refDoc.id}`);
+      return refDoc.id
+
+    } catch (error) {
+      console.error(`Error creando la entrada => ${error}`)
+    }
+  }
+
+  async crearSalida(
+    uid,
+    idParqueadero,
+    nombreCliente,
+    placa,
+    celularCliente,
+    observacion) {
+    try {
+      const refDoc = await this.db
+        .collection('salidas')
+        .add({
+          uid: uid,
+          idparqueadero: idParqueadero,
+          nombrecliente: nombreCliente,
+          celularcliente: celularCliente,
+          placa: placa,
+          observacion: observacion,
+          fecha: firebase.firestore.FieldValue.serverTimestamp()
+        })
+
+      console.log(`Id de la salida => ${refDoc.id}`)
+      return refDoc.id
+
+    } catch (error) {
+      console.error(`Error creando la salida => ${error}`)
+    }
+  }
+
+  async consultarTodosParquedaderos(fntCallBack) {
+    try {
+      await this.db
+        .collection('parqueaderos')
+        .orderBy('nombre', 'asc')
+        .onSnapshot((parqueaderos) => {     
+          
+          $('#parqueaderos').empty();
+
+          parqueaderos.forEach(async (parqueadero) => {
+
+            const parqueaderoData = parqueadero.data();
+
+            const entradasParqueadero = await this.db
+              .collection('entradas')
+              .where('idparqueadero', '==', parqueadero.id)
+              .orderBy('fecha', 'desc')
+              .limit(1)
+              .get();
+
+            if (entradasParqueadero.empty || parqueaderoData.libre === true) {
+              fntCallBack({
+                nombreParqueadero: parqueaderoData.nombre,
+                libre: parqueaderoData.libre,
+                id: parqueadero.id
+              });
+            } else {
+              const entradaParqueadero = entradasParqueadero.docs[0].data();
+              fntCallBack({
+                nombreParqueadero: parqueaderoData.nombre,
+                libre: parqueaderoData.libre,
+                nombreCliente: entradaParqueadero.nombrecliente,
+                celularCliente: entradaParqueadero.celularcliente,
+                placa: entradaParqueadero.placa,
+                observacion: entradaParqueadero.observacion,
+                imagenLink: entradaParqueadero.imagenlink,
+                fecha: entradaParqueadero.fecha,
+                id: parqueadero.id
+              });
+            }
+
+          });
+
+        });
+
+    } catch (error) {
+      console.error(`Error consultando todos los parqueaderos => ${error}`)
+    }
+  }
+
+  async consultarParqueaderoLibres(fntCallBack) {
+    try {
+      await this.db
+        .collection('parqueaderos')
+        .orderBy('nombre', 'asc')
+        .where('libre', '==', true)
+        .onSnapshot((parqueaderos) => {   
+          
+          $('#parqueaderos').empty();
+
+          parqueaderos.forEach((parqueadero) => {
+            const parqueaderoData = parqueadero.data();
+            console.log(parqueaderoData);
+            fntCallBack({
+              nombreParqueadero: parqueaderoData.nombre,
+              libre: parqueaderoData.libre,
+              id: parqueadero.id
+            });
+
+          });
+
+        });
+
+    } catch (error) {
+      console.error(`Error consultando todos los parqueaderos libres => ${error}`)
+    }
+  }
+
+  subirImagenPost(file, uid) {
+    const refStorage = firebase.storage().ref(`imgsParqueaderos/${uid}/${file.name}`);
+    const task = refStorage.put(file)
+
+    task.on(
+      'state_changed',
+      snapshot => {
+        const porcentaje = snapshot.bytesTransferred / snapshot.totalBytes * 100
+        $('.determinate').attr('style', `width: ${porcentaje}%`)
+      },
+      err => {
+        Materialize.toast(`Error subiendo archivo = > ${err.message}`, 4000)
+      },
+      async () => {
+        try {
+          const url = await task.snapshot.ref
+            .getDownloadURL();
+
+          console.log(url)
+          sessionStorage.setItem('imgNewEntrada', url)
+
+        } catch (err) {
+          Materialize.toast(`Error obteniendo downloadURL = > ${err}`, 4000)
+        }
+      }
+    )
+  }
+
+
+}
